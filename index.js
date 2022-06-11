956c7ada192502012a5cfa5256f8bb49abbee832
Burak Tokak <iletisim@buraktokak.com>
2022-06-11 19:00:25 +0300

    cheanges

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
 const chromium = require("chrome-aws-lambda");
 const lighthouse = require("lighthouse");
 
-// The important bit, set the path to the chromium binary and expose it on the export too, just in case.
-lighthouse.CHROME_PATH = process.env.CHROME_PATH = chromium.path;
-lighthouse.installChrome = chromium.install;
 
-// Allow programmatic use
-module.exports = lighthouse;
+module.exports = async(url, options) => {
+  let browser = await chromium.puppeteer.launch({
+    args: [...chromium.args, "--remote-debugging-port=9222"],
+    defaultViewport: chromium.defaultViewport,
+    executablePath: await chromium.executablePath,
+    headless: chromium.headless,
+    ignoreHTTPSErrors: true,
+  });
+
+  options.port = 9222;
+
+  return await lighthouse(url, options);
+};
